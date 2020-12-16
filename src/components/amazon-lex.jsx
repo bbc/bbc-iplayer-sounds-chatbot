29a3ac52b22a37f0ddfdf0187f47eb52d7a6c546
Moudud Abu <32541410+MoududAbu@users.noreply.github.com>
2020-12-16 15:43:33 +0000

    clean up

--- src/components/amazon-lex.jsx
+++ src/components/amazon-lex.jsx
@@ -1,131 +1,103 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, {Component} from "react";
 import AWS from "aws-sdk";
 import "../styles/chatbot.css";
-import { Widget, addResponseMessage } from "react-chat-widget";
+import {Widget, addResponseMessage} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 
 class LexChat extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: "",
-      lexUserId: "chatbot" + Date.now(),
-      sessionAttributes: this.props.sessionAttributes,
-      visible: "closed",
-    };
-    this.pushChat = this.pushChat.bind(this);
-  }
-
-  componentDidMount() {
-    AWS.config.region = this.props.region || "us-west-2";
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: this.props.IdentityPoolId,
-    });
-    let lexruntime = new AWS.LexRuntime();
-    this.lexruntime = lexruntime;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.props.sessionAttributes &&
-      this.props.sessionAttributes !== prevState.sessionAttributes
-    ) {
-      this.state.sessionAttributes = {
-        ...this.state.sessionAttributes,
-        ...this.props.sessionAttributes,
-      };
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: "",
+            lexUserId: "chatbot" + Date.now(),
+            sessionAttributes: this.props.sessionAttributes,
+            visible: "closed",
+        };
+        this.pushChat = this.pushChat.bind(this);
     }
-  }
 
-  handleClick() {
-    this.setState({
-      visible: this.state.visible == "open" ? "closed" : "open",
-    });
-    if (this.props.debugMode === true) {
-      console.log(this.state);
+    componentDidMount() {
+        AWS.config.region = this.props.region || "us-west-2";
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+            IdentityPoolId: this.props.IdentityPoolId,
+        });
+        let lexruntime = new AWS.LexRuntime();
+        this.lexruntime = lexruntime;
     }
-  }
 
-  pushChat(event) {
-    let inputFieldText = event;
-
-    if (
-      inputFieldText &&
-      inputFieldText.trim().length > 0
-    ) {
-
-      console.log("🚀 ~ file: amazon-lex.jsx ~ line 79 ~ LexChat ~ pushChat ~ props", this.props)
-
-      // send it to the Lex runtime
-      let params = {
-        botAlias: this.props.alias,
-        botName: this.props.botName,
-        inputText: inputFieldText,
-        userId: this.state.lexUserId,
-        sessionAttributes: this.state.sessionAttributes,
-      };
-      console.log("🚀 ~ file: amazon-lex.jsx ~ line 78 ~ LexChat ~ pushChat ~ params", params)
-
-
-      if (this.props.debugMode === true) {
-        console.log(JSON.stringify(params));
-      }
-
-      // this.showRequest(inputField);
-      let a = function(err, data) {
-        if (err) {
-          console.log(err, err.stack);
-          this.showError(
-            "Error:  " + err.message + " (see console for details)"
-          );
+    componentDidUpdate(prevProps, prevState) {
+        if (
+            this.props.sessionAttributes &&
+            this.props.sessionAttributes !== prevState.sessionAttributes
+        ) {
+            this.state.sessionAttributes = {
+                ...this.state.sessionAttributes,
+                ...this.props.sessionAttributes,
+            };
         }
-        if (data) {
-          console.log("🚀 ~ file: amazon-lex.jsx ~ line 93 ~ LexChat ~ a ~ data", data)
-          // capture the sessionAttributes for the next cycle
-          this.setState({ sessionAttributes: data.sessionAttributes });
-          // show response and/or error/dialog status
-          addResponseMessage(data.message);
-        }
-      };
-
-      this.lexruntime.postText(params, a.bind(this));
     }
-    // we always cancel form submission
-    return false;
-  }
 
-  handleChange(event) {
-    event.preventDefault();
-    this.setState({ data: event.target.value });
-  }
+    pushChat(event) {
+        let inputFieldText = event;
 
-  render() {
-    return <Widget handleNewUserMessage={this.pushChat} />;
-  }
+        if (
+            inputFieldText &&
+            inputFieldText.trim().length > 0
+        ) {
+
+            // send it to the Lex runtime
+            let params = {
+                botAlias: this.props.alias,
+                botName: this.props.botName,
+                inputText: inputFieldText,
+                userId: this.state.lexUserId,
+                sessionAttributes: this.state.sessionAttributes,
+            };
+
+            if (this.props.debugMode === true) {
+                console.log(JSON.stringify(params));
+            }
+
+            // this.showRequest(inputField);
+            let a = function (err, data) {
+                if (err) {
+                    console.log(err, err.stack);
+                    this.showError(
+                        "Error:  " + err.message + " (see console for details)"
+                    );
+                }
+                if (data) {
+                    console.log("🚀 ~ file: amazon-lex.jsx ~ line 93 ~ LexChat ~ a ~ data", data)
+                    // capture the sessionAttributes for the next cycle
+                    this.setState({sessionAttributes: data.sessionAttributes});
+                    // show response and/or error/dialog status
+                    addResponseMessage(data.message);
+                }
+            };
+
+            this.lexruntime.postText(params, a.bind(this));
+        }
+        // we always cancel form submission
+        return false;
+    }
+
+    render() {
+        const {title, subtitle, profileAvatar} = this.props;
+
+        return <Widget
+            profileAvatar={profileAvatar}
+            title={title}
+            subtitle={subtitle}
+            handleNewUserMessage={this.pushChat}/>
+    }
 }
 
-// LexChat.propTypes = {
-//   alias: PropTypes.string,
-//   botName: PropTypes.string,
-//   IdentityPoolId: PropTypes.string.isRequired,
-//   placeholder: PropTypes.string.isRequired,
-//   backgroundColor: PropTypes.string,
-//   height: PropTypes.number,
-//   headerText: PropTypes.string,
-//   headerColor: PropTypes.string,
-//   headerBackgroundColor: PropTypes.string,s
-//   headerFontSize: PropTypes.number,
-//   sessionAttributes: PropTypes.object,
-//   debugMode: PropTypes.bool,
-// };
-
 LexChat.defaultProps = {
-  alias: "$LATEST",
-  headerStyle: {},
-  greeting: "",
-  sessionAttributes: {},
-  debugMode: false,
+    alias: "$LATEST",
+    headerStyle: {},
+    greeting: "",
+    sessionAttributes: {},
+    debugMode: false,
 };
 
 export default LexChat;
--- src/components/chatBot.jsx
+++ src/components/chatBot.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-// import { ThemeProvider } from "styled-components";
-// import { iPlayerTheme } from "../themes/appThemes";
-// import { Widget, renderCustomComponent } from "react-chat-widget";
 import LexChat from "./amazon-lex";
 
 const ChatBotComponent = () => (
     <LexChat
-      botName="bbciPlayerSoundsChatbot"
-      IdentityPoolId="eu-west-2:e1f14c95-8241-493e-826c-4a4b6873ef88"
-      placeholder="Placeholder text"
-      backgroundColor="#FFFFFF"
-      height="430px"
-      region="eu-west-2"
-      headerText="Chat with our awesome bot"
-      headerStyle={{ backgroundColor: "#ABD5D9", fontSize: "30px" }}
-      greeting={
-        "Hello, how can I help? You can say things like 'help' to get more info"
-      }
-      alias='beta'
+        botName="bbciPlayerSoundsChatbot"
+        title="iPlayer Chatterbot"
+        subtitle="Welcome to iPlayer Chatbot"
+        profileAvatar={'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/82455870-4c7f-4e1d-bedb-661a3b4deb8b/d6xs5ii-d12ea164-7303-4c5b-ab94-2f0d28be822b.png'}
+        IdentityPoolId="eu-west-2:e1f14c95-8241-493e-826c-4a4b6873ef88"
+        region="eu-west-2"
+        headerText="Chat with our awesome bot"
     />
 );
 
